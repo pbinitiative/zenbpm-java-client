93a9728020acb69a6dcda28f9998e28c224098d0
triskill <borbot@email.cz>
2026-04-08 10:30:26 +0200

    remove PostConstruct due to Springboot cross-version compatibility

--- zenbpm-spring-boot-starter/src/main/java/org/zenbpm/rest/ZenbpmClientService.java
+++ zenbpm-spring-boot-starter/src/main/java/org/zenbpm/rest/ZenbpmClientService.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.zenbpm.ZenbpmClientProperties;
 import org.zenbpm.client.ApiClient;
 
-import javax.annotation.PostConstruct;
-
 public class ZenbpmClientService {
 
     private static final Logger log = LoggerFactory.getLogger(ZenbpmClientService.class);
@@ -20,6 +18,11 @@ public class ZenbpmClientService {
     private final boolean isOtelDisabled;
 
     private ApiClient apiClient;
+
+    public ApiClient getApiClient() {
+        initApiClient();
+        return apiClient;
+    }
 
     public ZenbpmClientService(
             ZenbpmClientProperties properties,
@@ -31,38 +34,31 @@ public class ZenbpmClientService {
         this.isOtelDisabled = isOtelDisabled;
     }
 
-    @PostConstruct
-    private void init() {
-        initApiClient();
-    }
-
-    public ApiClient getApiClient() {
-        return apiClient;
-    }
-
     private void initApiClient() {
-        apiClient = new ApiClient();
-        apiClient.setBasePath(properties.getRestUrl());
+        if (apiClient == null) {
+            apiClient = new ApiClient();
+            apiClient.setBasePath(properties.getRestUrl());
 
-        OkHttpClient.Builder builder = apiClient.getHttpClient().newBuilder();
+            OkHttpClient.Builder builder = apiClient.getHttpClient().newBuilder();
 
-        OpenTelemetry otel = openTelemetry.getIfAvailable();
-        if (!isOtelDisabled && otel != null) {
-            builder.addInterceptor(new ZenbpmOkHttpOtelInterceptor(otel));
+            OpenTelemetry otel = openTelemetry.getIfAvailable();
+            if (!isOtelDisabled && otel != null) {
+                builder.addInterceptor(new ZenbpmOkHttpOtelInterceptor(otel));
+            }
+
+            if (properties.isRestLoggingEnabled()) {
+                HttpLoggingInterceptor.Logger slf4jLogger = this::logHttpMessage;
+                HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(slf4jLogger);
+                interceptor.setLevel(
+                        log.isTraceEnabled()
+                                ? HttpLoggingInterceptor.Level.BODY
+                                : HttpLoggingInterceptor.Level.BASIC
+                );
+                builder.addInterceptor(interceptor);
+            }
+
+            apiClient.setHttpClient(builder.build());
         }
-
-        if (properties.isRestLoggingEnabled()) {
-            HttpLoggingInterceptor.Logger slf4jLogger = this::logHttpMessage;
-            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(slf4jLogger);
-            interceptor.setLevel(
-                    log.isTraceEnabled()
-                            ? HttpLoggingInterceptor.Level.BODY
-                            : HttpLoggingInterceptor.Level.BASIC
-            );
-            builder.addInterceptor(interceptor);
-        }
-
-        apiClient.setHttpClient(builder.build());
     }
 
     private void logHttpMessage(String message) {
